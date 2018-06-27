53f08d6284838c745acd5ec4302b301aa48cce1f
S3lvatico <s3lvatico@gmail.com>
2018-06-27 08:13:39 +0200

    non va mai bene

---
+++ src/main/java/org/gmnz/web/beans/AccessInfo.java
@@ -0,0 +1,55 @@
+package org.gmnz.web.beans;
+
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+import java.util.Date;
+
+
+public class AccessInfo implements HttpSessionBindingListener {
+
+	private Date sessionCreationDate;
+
+	private int accessCount;
+
+
+
+	public AccessInfo() {
+		accessCount = 1;
+	}
+
+
+
+	@Override
+	public void valueBound(HttpSessionBindingEvent httpSessionBindingEvent) {
+		final HttpSession session = httpSessionBindingEvent.getSession();
+		sessionCreationDate = new Date(session.getCreationTime());
+		System.out.printf("new value bound | name [%s] | value [%s]%n", httpSessionBindingEvent.getName(), httpSessionBindingEvent.getValue());
+	}
+
+
+
+	@Override
+	public void valueUnbound(HttpSessionBindingEvent httpSessionBindingEvent) {
+
+	}
+
+
+
+	public void updateAccessCount() {
+		accessCount++;
+	}
+
+
+
+	public int getAccessCount() {
+		return accessCount;
+	}
+
+
+
+	public Date getSessionCreationDate() {
+		return sessionCreationDate;
+	}
+}
